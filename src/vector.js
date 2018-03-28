6d8b13217b236fdba0c6df3b1a495995f2159605
sarang-s <email.sarangs@gmail.com>
2018-03-28 20:52:36 +0530

    subtract functions

--- src/vector.js
+++ src/vector.js
@@ -19,4 +19,20 @@ export default class Vector{
             return new Vector(a.x + b.x, a.y + b.y);
         }
     }
+
+    subtract(n){
+        if(n instanceof Number){
+            this.x -= n;
+            this.y -= n;
+        }else if(n instanceof Vector){
+            this.x -= n.x;
+            this.y -= n.y;
+        }
+    }
+
+    static subtract(a, b){
+        if(a instanceof Vector && b instanceof Vector){
+            return new Vector(a.x - b.x, a.y - b.y);
+        }
+    }
 }
