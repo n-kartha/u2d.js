db3a02046e1d84ee2bcb0f74902b0f796e7c1ff7
n-kartha <niranjankartha@gmail.com>
2018-03-29 12:41:17 +0530

    Fix JSDoc that said that methods returned strings instead of `Error` objects

--- src/errors.js
+++ src/errors.js
@@ -1,12 +1,12 @@
 /**
- * Error messages to be used by all functions in Universe2D. All methods return a string containing the error message.
+ * Error messages to be used by all functions in Universe2D. All methods return an Error containing the error message.
  */
 const errorMessages = {
   /**
    * Thrown when the arguments of a function are not the same as those required
    * @param {string[]} expected Expected argument types
    * @param {Object[]} found `arguments` object for the function
-   * @returns {string} Error message
+   * @returns {Error} Throwable error
    */
   invalidArguments(expected, found) {
     found = [].slice.call(found) // found is an `arguments` object which has to be converted to a regular array to call `map`
