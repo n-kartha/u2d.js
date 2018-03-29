05ca3be6fe1f857acf5d2f3582c0b884f7f13627
n-kartha <niranjankartha@gmail.com>
2018-03-29 11:14:59 +0530

    Edit to follow style guide

--- src/vector.js
+++ src/vector.js
@@ -1,38 +1,38 @@
-export default class Vector{
-    constructor(x,y){
-        this.x = x;
-        this.y = y;
-    }
+export default class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    add(n){
-        if(n instanceof Number){
-            this.x += n;
-            this.y += n;
-        }else if(n instanceof Vector){
-            this.x += n.x;
-            this.y += n.y;
-        }
+  add(n) {
+    if (n instanceof Number) {
+      this.x += n;
+      this.y += n;
+    } else if (n instanceof Vector) {
+      this.x += n.x;
+      this.y += n.y;
     }
+  }
 
-    static add(a, b){
-        if(a instanceof Vector && b instanceof Vector){
-            return new Vector(a.x + b.x, a.y + b.y);
-        }
+  static add(a, b) {
+    if (a instanceof Vector && b instanceof Vector) {
+      return new Vector(a.x + b.x, a.y + b.y);
     }
+  }
 
-    subtract(n){
-        if(n instanceof Number){
-            this.x -= n;
-            this.y -= n;
-        }else if(n instanceof Vector){
-            this.x -= n.x;
-            this.y -= n.y;
-        }
+  subtract(n) {
+    if (n instanceof Number) {
+      this.x -= n;
+      this.y -= n;
+    } else if (n instanceof Vector) {
+      this.x -= n.x;
+      this.y -= n.y;
     }
+  }
 
-    static subtract(a, b){
-        if(a instanceof Vector && b instanceof Vector){
-            return new Vector(a.x - b.x, a.y - b.y);
-        }
+  static subtract(a, b) {
+    if (a instanceof Vector && b instanceof Vector) {
+      return new Vector(a.x - b.x, a.y - b.y);
     }
+  }
 }
