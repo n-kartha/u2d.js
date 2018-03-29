7ae2a6ebf02aea58ded617a8919e5a9318a6dd7e
n-kartha <niranjankartha@gmail.com>
2018-03-29 11:21:28 +0530

    Lint and fix stuff and added error messages

--- src/vector.js
+++ src/vector.js
@@ -1,38 +1,59 @@
-export default class Vector {
+import errorMessages from './errors';
+
+/**
+ * Class to hold 2-dimensional vectors
+ */
+class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
 
-  add(n) {
-    if (n instanceof Number) {
-      this.x += n;
-      this.y += n;
-    } else if (n instanceof Vector) {
-      this.x += n.x;
-      this.y += n.y;
-    }
-  }
+  // add(n) {
+  //   if (typeof n === "number") {
+  //     this.x += n;
+  //     this.y += n;
+  //   } else if (n instanceof Vector) {
+  //     this.x += n.x;
+  //     this.y += n.y;
+  //   }
+  // }
 
+  /**
+   * Add 2 vectors
+   * @param {Vector} a First vector
+   * @param {Vector} b Second vector
+   * @returns {Vector} Mathematical equivalent of a + b
+   */
   static add(a, b) {
     if (a instanceof Vector && b instanceof Vector) {
       return new Vector(a.x + b.x, a.y + b.y);
+    } else {
+      throw errorMessages.invalidArguments(['Vector', 'Vector'], arguments);
     }
   }
 
-  subtract(n) {
-    if (n instanceof Number) {
-      this.x -= n;
-      this.y -= n;
-    } else if (n instanceof Vector) {
-      this.x -= n.x;
-      this.y -= n.y;
-    }
-  }
+  // subtract(n) {
+  //   if (typeof n === "number") {
+  //     this.x -= n;
+  //     this.y -= n;
+  //   } else if (n instanceof Vector) {
+  //     this.x -= n.x;
+  //     this.y -= n.y;
+  //   }
+  // }
 
+  /**
+   * Subtract a vector from another
+   * @param {Vector} a Vector to be subtracted from
+   * @param {Vector} b Vector to be subtracted
+   * @returns {Vector} Mathematical equivalent of a - b
+   */
   static subtract(a, b) {
     if (a instanceof Vector && b instanceof Vector) {
       return new Vector(a.x - b.x, a.y - b.y);
+    } else {
+      throw errorMessages.invalidArguments(['Vector', 'Vector'], arguments);
     }
   }
 }
