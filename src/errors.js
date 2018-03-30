993599661c126fe70e2e284d301c2b389c255d36
n-kartha <niranjankartha@gmail.com>
2018-03-30 09:27:51 +0530

    Add `notImplemented` error

--- src/errors.js
+++ src/errors.js
@@ -1,18 +1,25 @@
 /**
- * Error messages to be used by all functions in Universe2D. All methods return an Error containing the error message.
+ * Error messages to be used by all functions in Universe2D. All methods return an error object containing the error message.
  */
 const errorMessages = {
   /**
    * Thrown when the arguments of a function are not the same as those required
    * @param {string[]} expected Expected argument types
    * @param {Object[]} found `arguments` object for the function
-   * @returns {Error} Throwable error
+   * @returns {TypeError} Throwable error
    */
   invalidArguments(expected, found) {
     found = [].slice.call(found) // found is an `arguments` object which has to be converted to a regular array to call `map`
       .map(el => typeof el);
 
-    return Error(`Argument Error: Expected arguments [${expected.join(', ')}], found [${found.join(', ')}]`);
+    return new TypeError(`Argument Error: Expected arguments [${expected.join(', ')}], found [${found.join(', ')}]`);
+  },
+
+  /**
+   * Thrown when a function that should be implemented is not
+   */
+  notImplemented() {
+    return new Error('The function accessed has not been implemented by a derived class.');
   }
 };
 
