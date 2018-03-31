8068124fe2eaac3a078623947c8770965783600e
n-kartha <niranjankartha@gmail.com>
2018-03-31 18:23:24 +0530

    Fix imports and add error messages

--- src/universe.js
+++ src/universe.js
@@ -4,15 +4,21 @@ import {
 } from './main';
 
 import GameObject from './gameobject/gameobject';
-import erros from './dev/errors';
+import errors from './dev/errors';
 
 /**
- * Class for interacting with a HTML5 CanvasRendringContext2D
+ * Class for interacting with a HTML5 <code>CanvasRendringContext2D</code>
  */
 class Universe {
+  /**
+   * Initializes a new 2D Universe
+   * @param {Vector} dimensions Dimensions (width, height) of the canvas
+   */
   constructor(dimensions) {
     if (dimensions instanceof Vector) {
       this[priv].dim = dimensions;
+    } else {
+      throw errors.invalidArguments(['Vector'], arguments);
     }
   }
 }
