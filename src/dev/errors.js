bc0b07fe8dd1f818a038cb596108b216743578ff
n-kartha <niranjankartha@gmail.com>
2018-04-30 12:07:02 +0530

    Add `InvalidObjectError`

--- src/dev/errors.js
+++ src/dev/errors.js
@@ -8,24 +8,40 @@ const errors = {
    * Thrown when the arguments of a function are not the same as those required
    * @param {string[]} expected Expected argument types
    * @param {Object[]} found `arguments` object for the function
-   * @returns {TypeError} Throwable error
+   * @returns {Error} Throwable `ArgumentError`
    */
   invalidArguments(expected, found) {
-    found = [].slice.call(found) // found is an `arguments` object
+    found = [].slice.call(found) // `found` is an `arguments` object
       .map(el => typeof el);
 
-    let err = new Error(`Expected arguments [${expected.join(', ')}], found [${found.join(', ')}]`);
+    let err = new Error(`Expected arguments [${expected.join(', ')}], found [${found.join(', ')}]. Try reading the docs to make sure that you are calling the method correctly.`);
     err.name = 'ArgumentError';
 
     return err;
   },
 
   /**
-   * Thrown when a function that should be implemented is not
+   * Thrown when keys expected in an object are not present
+   * @param {string} objName Name of the invalid object
+   * @param {string} expected Expected keys
+   * @param {object} received Received keys
+   * @returns {Error} Throwable `InvalidObjectError`
+   */
+  invalidObject(objName, expected, received) {
+    let err = new Error(`Expected object ${objName} with keys ${expected}, found ${JSON.stringify(received)}. Try reading the docs to make sure that you're passing all objects correctly.`);
+    err.name = 'InvalidObjectError';
+
+    return err;
+  },
+
+  /**
+   * Thrown when a function that should be implemented is not implemented
+   * @returns {Error} Throwable `NotImplementedError`
    */
   notImplemented() {
-    let err = new Error('The function accessed has not been implemented by a derived class.');
+    let err = new Error('The function accessed has not been implemented by a derived class. You have to override a few functions if you want to inherit from the specific class.');
     err.name = 'NotImplementedError';
+
     return err;
   },
 
@@ -33,10 +49,12 @@ const errors = {
    * Thrown when an inexistent member is passed as an argument
    * @param {string} type Type of inexistent member
    * @param {string} list List from which the member is missing
+   * @returns {Error} Throwable `InexistentObjectError`
    */
   inexistent(type, list) {
     let err = new Error(`The ${type} specified does not exist in the current ${list}.`);
     err.name = 'InexistentObjectError';
+
     return err;
   }
 };
